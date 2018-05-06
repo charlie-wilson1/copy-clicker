801e505a5db411d5643f8a911d7e4d8630a63aa0
johnxferraro <johnxferraro@gmail.com>
2018-05-06 16:11:51 -0600

    add the register functionality, test via postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,9 +4,58 @@ const express = require('express');
 
 const router = express.Router();
 
-// @route   GET api/posts/test
+const gravatar = require('gravatar');
+
+const bcrypt = require('bcryptjs');
+
+// load user model
+const User = require('../../models/User');
+
+// @route   GET api/users/test
 // @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'users works!' }));
+
+// @route   POST api/users/register
+// @desc    Register a user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(404).json({ email: 'Email already exists' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+  // .catch(err => {
+  //   console.log('error!');
+  // });
+});
+
+// @route   POST api/users/login
+// @desc    Login a user
+// @access  Public
+router.post('/login', (req, res) => {});
 
 module.exports = router;

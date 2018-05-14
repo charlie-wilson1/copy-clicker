8d75af0c1d28bd424b26dbbb4561cd658e7570f6
johnxferraro <johnxferraro@gmail.com>
2018-05-13 20:29:12 -0600

    start the homepage design

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,56 +1,90 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Typist from 'react-typist';
+import TypistLoop from 'react-typist-loop';
 
 class Landing extends Component {
+  // return (
+  constructor() {
+    super();
+    // const words = ['one', 'two', 'three'];
+  }
+
+  // )
+
   render() {
     return (
       <div>
-        <div className="landing">
-          <div className="dark-overlay landing-inner text-light">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">
-                    Focus on Selling, Not on Writing Copy.
-                  </h1>
-                  <p className="lead">Handcrafted Copy is just a click away.</p>
-                  <hr />
-                  <Link to="/register" className="btn btn-lg btn-info mr-2">
-                    Sign Up
-                  </Link>
-                  <Link to="/login" className="btn btn-lg btn-light">
-                    Login
-                  </Link>
-                </div>
+        {/* <div className="landing"> */}
+        {/* <div className="dark-overlay landing-inner text-light"> */}
+        <section class="my-5">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 mb-4 text-left">
+                  Handcrafted & Proven:{' '}
+                  <TypistLoop interval={1000}>
+                    {['Emails', 'Sequences', 'SMS', 'Scripts', 'Copy'].map(
+                      text => (
+                        <Typist key={text} startDelay={1000}>
+                          {text}
+                        </Typist>
+                      )
+                    )}
+                  </TypistLoop>
+                </h1>
+                <p className="lead text-left">
+                  Focus on selling. Not on writing.
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg mr-2 btn-2-signup">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light btn-2-login">
+                  Login
+                </Link>
               </div>
             </div>
+            {/* </div> */}
+            {/* </div> */}
           </div>
-        </div>
+        </section>
 
-        <section>
+        <section className="bg-light bg-dark section-2">
           <div className="container">
             <div className="row">
               <div className="col-md-4">
                 <img
                   src={window.location.origin + '/img/bulletin_board.png'}
-                  className="mx-auto d-block"
-                  max-width="256px"
+                  className="mx-auto d-block home-img"
                 />
                 <h2 className="text-center">Click.</h2>
+                <p className="lead">
+                  Click through our library of over 100 pre-written copy
+                  segments. More are added every week.
+                </p>
               </div>
               <div className="col-md-4">
                 <img
                   src={window.location.origin + '/img/pencil_tip.png'}
-                  className="mx-auto d-block"
+                  className="mx-auto d-block home-img-2"
                 />
                 <h2 className="text-center">Customize.</h2>
+                <p className="lead">
+                  Customize the copy you choose to cater it towards your
+                  audience. Use our AI tips to streamline your process.
+                </p>
               </div>
               <div className="col-md-4">
                 <img
                   src={window.location.origin + '/img/lightbulb_idea.png'}
-                  className="mx-auto d-block"
+                  className="mx-auto d-block home-img-2"
                 />
                 <h2 className="text-center">Copy.</h2>
+                <p className="lead">
+                  Copy your copy into your pre-existing tools. Sell more with
+                  smarter copy.
+                </p>
               </div>
             </div>
           </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-black mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
             CopyClicker
@@ -30,12 +30,12 @@ class Navbar extends Component {
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/register">
+                <Link className="nav-link btn-signup" to="/register">
                   Sign Up
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/login">
+                <Link className="nav-link btn-login" to="/login">
                   Login
                 </Link>
               </li>

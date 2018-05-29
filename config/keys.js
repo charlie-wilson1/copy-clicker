3117ef025d569adba1a591ab0c18e0e10dc5197f
johnxferraro <johnxferraro@gmail.com>
2018-05-29 13:38:25 -0600

    update keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://joey:TjnPS3Jwls9r@ds111478.mlab.com:11478/copy-clicker',
-  secretOrKey: 'c34c34secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}

91894110990f9aa29b98512a3503757e161ec08f
johnxferraro <johnxferraro@gmail.com>
2018-05-29 15:34:13 -0600

    fix redux dev tools error

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,17 @@ import rootReducer from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
+// eslint-disable-next-line no-underscore-dangle
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+  devTools = a => a;
+}
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware), devTools)
 );
 
 export default store;
